d3bafac2484bc5d707b5bad3d87983ca40b54330
Nguyen Xuan Tuan <58553278+tuancto2000@users.noreply.github.com>
2021-06-21 20:33:14 +0700

    them table lichthi, hocvienCC

---
+++ DTO/HocVienCC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HocVienCC:HocVien
+    {
+        public string LoaiChungChi { get; set; }
+        public HocVienCC(string idHocVien, string idLop, string tenHocVien, DateTime ngaySinh, string email, string sDT, string diaChi, string loaiChungChi)
+            : base(idHocVien, idLop, tenHocVien, ngaySinh, email, sDT, diaChi)
+        {
+            LoaiChungChi = loaiChungChi;
+        }
+    }
+}
---
+++ DTO/LichThi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LichThi
+    {
+
+        public string IdMonHoc { get; set; }
+        public string IdGVCoiThi { get; set; }
+        public DateTime NgayThi { get; set; }
+        public string PhongThi { get; set; }
+
+        public LichThi(string idMonHoc, string idGVCoiThi, DateTime ngayThi, string phongThi)
+        {
+            IdMonHoc = idMonHoc;
+            IdGVCoiThi = idGVCoiThi;
+            NgayThi = ngayThi;
+            PhongThi = phongThi;
+        }
+    }
+}
