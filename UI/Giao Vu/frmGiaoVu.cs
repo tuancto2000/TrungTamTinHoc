7b2192614e4bb5e9b4858d82d991a5ff57a0aa59
aahhqqphuc <55779423+aahhqqphuc@users.noreply.github.com>
2021-06-22 14:29:06 +0700

    giáo vụ

---
+++ UI/Giao Vu/frmGiaoVu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class frmGiaoVu : Form
+    {
+        public frmGiaoVu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPhanCongGiangDay_Click(object sender, EventArgs e)
+        {
+            frmPhanCongGiangDay frmPhanCongGiangDay = new frmPhanCongGiangDay();
+            this.Hide();
+            frmPhanCongGiangDay.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_XepLoai_Click(object sender, EventArgs e)
+        {
+            frmXepLoai_CapChungChi frmXepLoai_CapChungChi = new frmXepLoai_CapChungChi();
+            this.Hide();
+            frmXepLoai_CapChungChi.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_ToChucThi_Click(object sender, EventArgs e)
+        {
+            frmToChucThi frmToChucThi = new frmToChucThi();
+            this.Hide();
+            frmToChucThi.ShowDialog();
+            //this.Show();
+            this.Close();
+        }
+    }
+}
---
+++ UI/Giao Vu/frmToChucThi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class frmToChucThi : Form
+    {
+        public frmToChucThi()
+        {
+            InitializeComponent();
+        }
+
+        private void frmToChucThi_Load(object sender, EventArgs e)
+        {
+            tb_mamh.Enabled = false;
+            tb_tenmh.Enabled = false;
+            btn_xacNhan.Enabled = false;
+        }
+
+        private void lv_mh_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_xacNhan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_huy_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
